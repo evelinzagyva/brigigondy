9445e473ca4feeeb5e53608c0810b1916f88f26e
evelinzagyva <evelinzagyva@gmail.com>
2021-03-21 14:14:57 +0100

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
